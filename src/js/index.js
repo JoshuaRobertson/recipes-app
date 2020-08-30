52d734641dd3583ac2db646e27a92861fcbc2544
JoshuaRobertson <josh.robertson@coinclear.uk>
2020-08-30 15:08:34 +0100

    Initial logic for recipeView with array of ingredients and instructions

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of app
@@ -70,7 +71,8 @@ const controlRecipe = async () => {
 
   if (id) {
     // 1. Prepare UI for changes
-
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
 
     // 2. Create new recipe object
     state.recipe = new Recipe(id);
@@ -85,7 +87,9 @@ const controlRecipe = async () => {
       state.recipe.calcServings();
 
       // 5. Render recipe
-      console.log(state.recipe);
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
+
     } catch (error) {
       alert('Error processing recipe.');
     }
